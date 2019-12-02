4f2a6372e3e8d62fb8c9078602b047d7db398c51
Mitchell <jmitchell@grupoassa.com>
2019-12-02 15:27:30 -0300

    Adding a json post test to /videogames path

--- src/test/java/VideoGameDBTests.java
+++ src/test/java/VideoGameDBTests.java
@@ -1,9 +1,10 @@
 import config.EndPoint;
+import config.TestConfig;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
 
-public class VideoGameDBTests {
+public class VideoGameDBTests extends TestConfig {
 
     @Test
     public void getAllGames(){
@@ -11,4 +12,23 @@ public class VideoGameDBTests {
         .when().get(EndPoint.VIDEOGAMES)
         .then();
     }
+
+    @Test
+    public void createNewGameByJSON(){
+        String gameBodyJson = "{\n" +
+                "  \"id\": 16,\n" +
+                "  \"name\": \"Mario Bros\",\n" +
+                "  \"releaseDate\": \"2019-12-02T18:13:58.789Z\",\n" +
+                "  \"reviewScore\": 50,\n" +
+                "  \"category\": \"Arcade\",\n" +
+                "  \"rating\": \"Everyone\"\n" +
+                "}";
+        given()
+                .body(gameBodyJson)
+                .log().all()
+        .when().post(EndPoint.VIDEOGAMES)
+        .then()
+                .log().all();
+    }
+
 }

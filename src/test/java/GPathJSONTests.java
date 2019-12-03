74b42accd3db48d02761e6e1951b649b952f91e7
Mitchell <jmitchell@grupoassa.com>
2019-12-03 13:04:52 -0300

    Adding a test that it returns the player names of a squad.

--- src/test/java/GPathJSONTests.java
+++ src/test/java/GPathJSONTests.java
@@ -2,17 +2,32 @@ import config.TestConfig;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.get;
 
 public class GPathJSONTests extends TestConfig {
+
     @Test
     public void extractMapOfElementWithFind() {
         Response response = get("/competitions/2013/teams");
-
         Map<String, ?> allTeamDataForSingleTeam = response.path("teams.find { it.name == 'Cruzeiro EC' }");
-
         System.out.println(allTeamDataForSingleTeam);
+    }
+
+    @Test
+    public void extractSingleValueWithFind(){
+        Response response = get("/teams/57");
+        String certainPlayer = response.path("squad.find {it.shirtNumber == 16}.name");
+        System.out.println("Player with shirt number 16 is " + certainPlayer);
+    }
+
+    @Test
+    public void extractMultipleValuesWithFind(){
+        Response response = get("/teams/57");
+        List<String> squadPlayersName = response.path("squad.findAll {it.role == 'PLAYER'}.name");
+        System.out.println("Players in the squad: ");
+        squadPlayersName.stream().forEach(playerName -> System.out.println(playerName));
     }
 }
